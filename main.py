54bf12f50284a765e69666805cb9d8c3680df46a
aydnylmz <aydin@marketyo.com>
2020-12-20 22:06:01 +0300

    scores and result plots

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from sample.dataset import load_transformed_car_data, plot_feature_importance
 from sample.machine_learning import split_test_and_train, linear_fit
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 
 def main():
@@ -12,7 +14,14 @@ def main():
 
     model_sets = split_test_and_train(X, y)
 
-    linear_fit(model_sets)
+    reg = LinearRegression()
+
+    linear_fit(reg, X, y, model_sets, 3)
+    linear_fit(reg, X, y, model_sets, 4)
+    linear_fit(reg, X, y, model_sets, 5)
+    linear_fit(reg, X, y, model_sets, 6)
+
+    print('k=5 has the smallest mean squared error')
 
 
 if __name__ == "__main__":
--- sample/machine_learning.py
+++ sample/machine_learning.py
@@ -1,6 +1,9 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+from sklearn.model_selection import cross_val_score
+import seaborn as sns
+import numpy as np
+from matplotlib import pyplot as plt
 
 
 def split_test_and_train(x, y):
@@ -11,10 +14,25 @@ def split_test_and_train(x, y):
             "y_train": y_train, "y_test": y_test}
 
 
-def linear_fit(model_sets):
-    reg = LinearRegression()
-    reg.fit(model_sets['X_train'], model_sets['y_train'])
-    y_pred = reg.predict(model_sets['X_test'])
+def linear_fit(model, x, y, model_sets, k=5):
+    model.fit(model_sets['X_train'], model_sets['y_train'])
+    y_pred = model.predict(model_sets['X_test'])
 
     rsqrd = r2_score(model_sets['y_test'], y_pred)
+    print(f'k = {k} (Cross-validation splitting)')
     print(f'R2 Error: {rsqrd}')
+
+    scores = cross_val_score(
+        model, x, y, scoring='neg_mean_squared_error', cv=k)
+    rmse = np.sqrt(-scores)
+    print('Reg rmse:', rmse)
+    print('Reg Mean:', rmse.mean())
+    print('---------------------------------------')
+
+    plt.figure(figsize=(18, 8))
+    sns.histplot(model_sets['y_test'] - y_pred)
+    plt.savefig('docs/distplot.png')
+
+    plt.figure(figsize=(10, 5))
+    plt.scatter(model_sets['y_test'], y_pred)
+    plt.savefig('docs/scatter.png')
